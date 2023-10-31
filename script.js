71b371d6e30c99bae1b6907c80890cd8585803d5
Asperity <145687068+Asperity@users.noreply.github.com>
2023-10-31 10:59:35 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -11,53 +11,64 @@ function initialize(){
     powerDisplay = document.getElementById("power");
     speedDisplay = document.getElementById("speed");
     siDisplay = document.getElementById("si");
+    display();
 }
 function increasePower(){
     if(powerLevel<powerLevelCap)
     {
         powerLevel++;
     }
+    display();
 }
+
 
 function decreasePower(){
     if(powerLevel>0)
     {
         powerLevel--;
     }
+    display();
 }
+
 
 function increaseSpeed(){
     if(topSpeed<topSpeedCap)
     {
         topSpeed++;
     }
+    display();
 }
+
 
 function decreaseSpeed(){
     if(topSpeed>0)
     {
         topSpeed--;
     }
-
+    display();
 }
+
 
 function increaseSI(){
     if(structuralIntegrity<structuralIntegrityCap)
     {
         structuralIntegrity++;
     }
+    display();
 }
+
 
 function decreaseSI(){
     if(structuralIntegrity>0)
     {
         structuralIntegrity--;
     }
-
+    display();
 }
 
+
 function display(){
-    powerDisplay.innerHTML = powerLevel;
-    speedDisplay.innerHTML = topSpeed;
-    siDisplay.innerHTML = structuralIntegrity;
+    powerDisplay.innerHTML = "POWER LEVEL: "+powerLevel;
+    speedDisplay.innerHTML = "SPEED: "+topSpeed;
+    siDisplay.innerHTML = "SI: "+structuralIntegrity;
 }
